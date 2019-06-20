b337a9641e87786073318ea45f99285c4f31d9cb
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-19 20:12:42 -0500

    Short-Lnk: End of video 54

--- client/main.js
+++ client/main.js
@@ -1,10 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-import Signup from './../imports/ui/Signup';
+import Login from './../imports/ui/Login';
 import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+import Signup from './../imports/ui/Signup';
+
+const routes = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Login}/>
+        <Route path="/links" component={Link}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="*" component={NotFound}/>
+    </Router>
+);
 
 Meteor.startup(() => {
-    ReactDOM.render(<Link/>, document.getElementById('app'));
+    ReactDOM.render(routes, document.getElementById('app'));
 });

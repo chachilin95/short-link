ed42c253e0f6c316a3324656da4cc85181bd4e5f
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-21 17:27:33 -0500

    Short Lnk: End of Video 56

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,12 +2,36 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: ''
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        // throwing an error just because
+        this.setState({
+            error: 'OH NO!'
+        });
+    }
+    
     render() {
         return (
             <div>
                 <h1>Signup for Short Lnk</h1>
-
-                signup form goes here
+                
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type='email' name='email' placeholder='abc@123.com'/>
+                    <input type='password' name='password' placeholder='Password'/>
+                    <button>Create account</button>
+                </form>
 
                 <Link to="/">Already have an account?</Link>
             </div>
